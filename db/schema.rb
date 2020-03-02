b0d4c1e84893da35911303110465277201e3a989
Daniel Kang <hk3021@columbia.edu>
2020-03-01 21:57:36 -0500

    adding the modifed things

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200229201132) do
+ActiveRecord::Schema.define(version: 20200302023833) do
 
   create_table "subjects", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tutors", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
