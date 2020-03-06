0b7785721e0632721336cf6cc44ae5c1444c821a
Daniel Kang <hk3021@columbia.edu>
2020-03-06 16:48:19 -0500

    fixed the /tutors link

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -1,7 +1,6 @@
 class TutorsController < ApplicationController
 	def index
-        @account = Account.find_by_id(session[:account_id])
-        @subjects = @account.tutor.subjects
+        @tutors = Tutor.all
     end
 
     def show
