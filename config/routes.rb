b6d8fd38fbe03c156bab9f8621ea32c73a6dcda2
Daniel Kang <hk3021@columbia.edu>
2020-03-30 17:38:19 -0400

    commiting

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,11 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   #resources :tutors
+    #
+
+  get '/approve/:id', to: 'sessions#approve', as: 'approve'
+  get 'reject/:id', to: 'sessions#reject', as: 'reject'
+
   resources :students
   resources :accounts
   resources :tutors
