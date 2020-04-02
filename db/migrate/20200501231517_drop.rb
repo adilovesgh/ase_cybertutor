291bdf9ff54d593df6177a65f7693590c6187372
Aditya Sridhar <adityasridhar@mott.cs.columbia.edu>
2020-04-02 18:33:02 -0400

    Removed unnecessary drop migration file

--- db/migrate/20200501231517_drop.rb
+++
@@ -1,7 +0,0 @@
-class Drop < ActiveRecord::Migration
-  def change
-    remove_column :tutors, :rate
-    remove_column :sessions, :rate
-    add_monetize :tutors, :price, currency: { present: false }
-  end
-end
