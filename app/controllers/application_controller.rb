2c9096447752696e8efed83f37a8aa454309f76b
Nick Bethune <nickbethune@hotmail.com>
2020-04-03 14:47:14 -0400

    fix accounts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :logged_in?
 
+  # before_filter:set_cache_buster
+
+  # def set_cache_buster
+  #   response.headers["Cache-control"] = "no-cache, no-store, max-age=0, must-revalidate"
+  #   response.headers["Pragma"] = "no-cache"
+
   def current_user
     Account.find_by(id: session[:account_id])
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200501231517) do
+ActiveRecord::Schema.define(version: 20200401231517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -115,8 +115,8 @@ ActiveRecord::Schema.define(version: 20200501231517) do
     t.integer  "experience"
     t.float    "average_rating"
     t.integer  "account_id"
-    t.string   "precision"
     t.integer  "price_cents",    default: 0, null: false
+    t.string   "precision"
   end
 
   add_index "tutors", ["account_id"], name: "index_tutors_on_account_id", using: :btree
