6c5944d73188e2b2066c391e56ed3848e7596e73
Daniel Kang <hk3021@columbia.edu>
2020-04-03 18:13:34 -0400

    reverting for heroku

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -61,17 +61,21 @@ class SessionsController < ApplicationController
                 @sessions = @account.student.sessions
                 if Session.no_student_conflicting_times(@time, @sessions)
                     @tutor = Tutor.find(params[:tutor_id])
+                    @student = @account.student
                     @price = Session.compute_session_cost(@tutor.price_cents, params["session"])
                     @subject = Subject.find(params[:subject_id])
-                    session[:tutor] = @tutor
-                    session[:subject] = @subject
-                    session[:start_time] = @start_time
-                    session[:end_time] = @end_time
-                    session[:price] = @price
-                    session[:price_cents] = @price * 100
+                    @session = @tutor.sessions.build(subject:@subject, student:@student, price:@price, start_time:@start_time, end_time:@end_time, pending:true, verified:false)
+                    #session[:tutor] = @tutor
+                    #session[:subject] = @subject
+                    #session[:start_time] = @start_time
+                    #session[:end_time] = @end_time
+                    #session[:price] = @price
+                    #session[:price_cents] = @price * 100
 
-                    redirect_to :controller=>"orders",
-                                :action=>"index"
+                    #redirect_to :controller=>"orders",
+                                #:action=>"index"
+                    @session.save
+                    redirect_to subject_tutor_sessions_path(1,1)
                 else
                     flash[:error] = "You already have a session conflicting with this!"
                     redirect_to new_subject_tutor_session_path
